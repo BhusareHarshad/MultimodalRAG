a172624e5d72419dad83182ae7525e978dedea34
Harshad Bhusare <harshadbhusare022@gmail.com>
2024-09-15 02:05:34 +0530

    font change

--- mm_frontend/src/App.jsx
+++ mm_frontend/src/App.jsx
@@ -5,32 +5,33 @@ import makeinindia from './assets/mii.png'
 
 function App() {
   return (
-  <>
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex flex-col items-center justify-center flex-grow p-4">
-        <img src={makeinindia} alt="Logo" className="mb-4" />
-        <h2 className="text-center text-2xl font-bold mb-4 text-black">
-          Effortless Transcription, Instant Queries, and Seamless Conversations
-        </h2>
-        <h2 className="text-center text-2xl font-bold mb-4 text-blue-700">
-          All in One Click!
-        </h2>
-        <input
-          type="text"
-          id="custom-input"
-          required
-          placeholder="Enter URL..."
-          className="mb-4 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <button className="button-custom">
-          Generate
-        </button>
-      </main>
-      <Footer />
-    </div>
-  </>
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex flex-col items-center justify-center flex-grow p-4">
+          <img src={makeinindia} alt="Logo" className="mb-4" />
+          <h2 className="text-center text-2xl font-semibold mb-4 text-black font-sans">
+            Effortless Transcription, Instant Queries, and Seamless Conversations
+          </h2>
+          <h2 className="text-center text-2xl font-semibold mb-4 text-blue-700 font-sans">
+            All in One Click!
+          </h2>
+          <input
+            type="text"
+            id="custom-input"
+            required
+            placeholder="Enter URL..."
+            className="mb-4 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <button className="button-custom">
+            Generate
+          </button>
+        </main>
+        <Footer />
+      </div>
+    </>
   );  
 }
+
 
 export default App;
